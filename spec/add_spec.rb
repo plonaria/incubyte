0bf31937a42d3e2a2cf0cf8515630bbb54c47f34
padmaja <padmajalonaria31@gmail.com>
2024-05-28 11:57:38 +0530

    add digit metho can handle any amount of number

--- spec/add_spec.rb
+++ spec/add_spec.rb
@@ -15,4 +15,10 @@ RSpec.describe Sum do
 		sum = Sum.new
 		expect(sum.add_digit(123)).to eq(6)
 	end
+
+	it 'can handle any amount of number' do 
+		sum = Sum.new
+		expect(sum.add_digit([12,123,1234])).to eq(19)
+	end
+
 end
