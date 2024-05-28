ceda159b7c72f882b3f0b45471631baf3fc45a67
padmaja <padmajalonaria31@gmail.com>
2024-05-28 11:43:44 +0530

    create add digit function

--- spec/add_spec.rb
+++ spec/add_spec.rb
@@ -1,9 +1,14 @@
 require './sum.rb'
 
 RSpec.describe Sum do 
-	it 'sum the given number' do 
+	it 'Class sum' do 
 		sum = Sum.new
-		num = '123'
-		expect(sum).to be(sum)
+		expect(sum).to be_kind_of(Sum)
 	end
+
+	it 'add digit function' do 
+		sum = Sum.new
+		expect(sum).to respond_to(:add_digit)
+	end
+
 end
--- sum.rb
+++ sum.rb
@@ -1,2 +1,5 @@
 class Sum
+
+	def add_digit
+	end
 end
